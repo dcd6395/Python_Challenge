eed3dda3041843f9ee18c62f08aade0b37cd5928
Daniella DeRose <dcd6395@gmail.com>
2019-12-03 18:27:52 -0600

    update

---
+++ PyBank/main.py
@@ -0,0 +1,8 @@
+import os
+import csv
+
+file_to_load = os.path.join("Resources", "budget_data.csv")
+file_to_output = os.path.join("PyBank", "PyBank_analysis.csv")
+
+with open(file_to_load, newline = '') as csvfile:
+    csvreader = csv.reader(csvfile, delimiter=",")
